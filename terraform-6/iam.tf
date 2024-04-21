37c2b41bfb53c6e842f3b228de5ca74391e80f4a
Seetaramaiah Thopuri <seetaramaiahthopuri@SeetaraaiahsAir.cust.communityfibre.co.uk>
2024-04-21 08:32:54 +0100

    iam and s3

---
+++ terraform-6/iam.tf
@@ -0,0 +1,10 @@
+module "iam_user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+
+  name          = "terraform-user"
+  force_destroy = true
+
+  pgp_key = "keybase:test"
+
+  password_reset_required = false
+}
